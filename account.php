fb67af2207bb2a5ccc4d75aee4f880f3aa4671ea
hug0be <83722580+hug0be@users.noreply.github.com>
2023-05-05 16:35:55 +0200

    [FEATURE] Added "aside" of account.php page

--- account.php
+++ account.php
@@ -1,9 +1,26 @@
-<?php require_once 'includes/header.php'; ?>
+<?php
+    require_once 'includes/header.php';
+    echo "<style>";
+    require_once 'assets/css/account.css';
+    echo "</style>";
+?>
 
 <div class="content">
     <section class="infos">
+        <aside class="ligth-frame">
+            <div class="btn-secondary">Mes livres</div>
+            <div class="separator"></div>
+            <div class="btn-secondary btn-disabled">Ma WishList</div>
+            <div class="separator"></div>
+            <div class="btn-secondary btn-disabled">Mes infos</div>
+            <div class="separator"></div>
+            <div class="btn-secondary btn-disabled">Mon cercle</div>
+        </aside>
+        <main class="ligth-frame"></main>
     </section>
 </div>
+
+<script src="assets/js/account.js" defer></script>
 
 <?php
 require_once 'includes/footer.php';
