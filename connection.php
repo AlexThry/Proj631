58f0c0cf00b52370c392174230b6c5bec02a7195
hug0be <83722580+hug0be@users.noreply.github.com>
2023-05-05 14:20:38 +0200

    [FEATURE] Connexion utilisateur

--- connection.php
+++ connection.php
@@ -17,20 +17,20 @@ require_once 'includes/header.php';
 		?>
 
 		<div class="field">
-			<label for="username">Username</label>
-			<input type="text" name="username" id="username-input-creation" placeholder="Username">
+			<label for="username">Nom d'utilisateur</label>
+			<input type="text" name="username" id="username-input-creation" placeholder="Nom d'utilisateur" <?php input_value("username") ?> >
 		</div>
 
 		<div class="field">
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password-input-creation" placeholder="Password">
+			<label for="password">Mot de passe</label>
+			<input type="password" name="password" id="password-input-creation" placeholder="Mot de passe">
 		</div>
 
 		<input type="submit" name="submit" id="submit-input-creation" value="Inscription">
 
-		<small>Vous n'avez pas encore de compte ? <a href="./subscription.php">Connectez-vous</a></small>
+		<small>Vous n'avez pas encore de compte ? <a href="./subscription.php">Inscrivez-vous</a></small>
 	</form>
-	
+
 </div>
 </seciton>
 
--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ function get_home_url(): string {
 function start_debugger(): void {
 	ini_set( 'display_errors', '1' );
 	ini_set( 'display_startup_errors', '1' );
-	error_reporting( E_ALL );
+	error_reporting(E_ALL);
 }
 
 /**
@@ -50,7 +50,41 @@ function start_debugger(): void {
  * @return void
  */
 function compute_connection( $username, $password ): void {
-	// add your code here ...
+	// Validate inputs
+	$errors = array();
+	if(empty($username)) $errors[] = "Insérez votre nom d'utilisateur";
+	if(empty($password)) $errors[] = "Insérez votre mot de passe";
+	if(!empty($errors)) {
+		foreach($errors as $error_message)  {
+			echo "<span class='error'>$error_message</span>";
+		}
+		return;
+	}
+
+	global $conn;
+	// Protect from XSS attack
+	$query = sprintf("SELECT * FROM user WHERE user_name='%s' LIMIT 1",
+		$conn->real_escape_string($username)
+	);
+
+	$result = $conn->query($query);
+	if (!$result) {
+		echo "<span class='error'>Erreur de connexion (".mysql_error().")</span>"; return;
+	}
+
+	$result = mysqli_fetch_assoc($result);
+	if(!$result) {
+		echo "<span class='error'>Ce compte n'existe pas</span>"; return;
+	}
+
+	$hash_password = hash("sha256", $password);
+	if($result['password'] !== $hash_password) {
+		echo "<span class='error'>Mot de passe incorrect</span>"; return;
+	}
+
+	echo "<span class='success'>Authentifié avec succès.</span>";
+
+	// TODO : Redirect to user page
 }
 
 /**
@@ -77,6 +111,16 @@ function compute_subscription( $username, $password, $confirm_password ): void {
 	} else {
 		echo "<span class='error'>Les mots de passe ne correspondent pas.</span>";
 	}
+}
+
+
+/**
+ * Returns a value field for html inputs
+ * Only if the input is in $_POST or $_GET
+ */
+function input_value($input): void {
+	if(isset($_POST[$input])) echo "value='".$_POST[$input]."'";
+	if(isset($_GET[$input])) echo "value='".$_GET[$input]."'";
 }
 
 /**
