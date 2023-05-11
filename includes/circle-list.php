3ee3294b314522a5afc4d9069a7d50b6d64d962d
Arnaud Pfundstein <arnaudpfundstein.pro@gmail.com>
2023-05-11 12:05:46 +0200

    Feature: add subscribe buttons for circles in circle.php

--- includes/circle-list.php
+++ includes/circle-list.php
@@ -15,15 +15,29 @@ $circles = Database::get_circles();
 				<?php foreach ( $circles as $circle ) : ?>
 					<li class="flex justify-between gap-x-6 py-5">
 						<div class="flex gap-x-4">
-						<img class="h-12 w-12 flex-none rounded-full bg-gray-50 dark:bg-gray-600" src="<?php echo key_exists( 'image_url', $circle ) ? $circle['image_url'] : 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'; ?>" alt="">
-						<div class="min-w-0 flex-auto">
-							<p class="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-100"><?php echo $circle['title']; ?></p>
-							<p class="mt-1 truncate text-xs leading-5 text-gray-500 dark:text-gray-400"><?php echo $circle['description']; ?></p>
+							<img class="object-cover h-12 w-12 flex-none rounded-full bg-gray-50 dark:bg-gray-600" src="<?php echo key_exists( 'image_url', $circle ) ? $circle['image_url'] : 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'; ?>" alt="">
+							<div class="min-w-0 flex-auto">
+								<p class="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-100"><?php echo $circle['title']; ?></p>
+								<p class="mt-1 truncate text-xs leading-5 text-gray-500 dark:text-gray-400"><?php echo $circle['description']; ?></p>
+							<p class="mt-1 text-xs leading-5 text-gray-500">Admin: <?php echo $circle['admin_first_name'] . ' ' . $circle['admin_last_name'] . ' (' . $circle['admin_user_name'] . ')'; ?></p>
+							</div>
 						</div>
-						</div>
+						
 						<div class="hidden sm:flex sm:flex-col sm:items-end">
-						<p class="text-sm leading-6 text-gray-900 dark:text-gray-200"><?php echo $circle['admin_user_name']; ?></p>
-						<p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z"><?php echo $circle['admin_first_name'] . ' ' . $circle['admin_last_name']; ?></time></p>
+						<?php
+						$is_abo = false;
+
+						if ( $is_abo ) :
+							?>
+							<a href="#" class="text-white bg-blue-700 inline-flex flex-align hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+								<svg fill="none" stroke="currentColor" stroke-width="1.5" class="w-5 h-5 mr-2 -ml-1"© viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+								</svg>
+								Abonné
+							</a>
+						<?php else : ?>
+							<a href="#" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">S'abonner</a>
+							<?php endif; ?>
 						</div>
 					</li>
 				<?php endforeach; ?>
