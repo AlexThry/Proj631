7d8afcebda3658381f60b3f096ab0c7370252803
Alexis Thierry <alexist103@gmail.com>
2023-05-13 13:35:40 +0200

    FEATURE : Merge -> Ajout commentaires

--- book.php
+++ book.php
@@ -7,9 +7,13 @@ if ( ! $book_id ) {
 	header( 'Location: ' . get_home_url() );
 	exit();
 }
-
-$has_read = Database::user_has_read(get_user()['id'], $book_id);
-$wants_to_read = Database::user_wants_to_read(get_user()['id'], $book_id);
+if (get_user() !== false) {
+	$has_read = Database::user_has_read(get_user()['id'], $book_id);
+	$wants_to_read = Database::user_wants_to_read(get_user()['id'], $book_id);
+} else {
+	$has_read = false;
+	$wants_to_read = false;
+}
 $book = Database::get_single_book( $book_id );
 ?>
 
@@ -20,13 +24,6 @@ $book = Database::get_single_book( $book_id );
 			  	<img class="w-lg rounded" src="<?php echo $book['image_url']; ?>" alt="<?php echo $book['title']; ?>">
 				<div class="mt-4 md:mt-0">
 					<h2 class="mb-2 text-5xl font-bold leading-none text-gray-900 md:text-5xl dark:text-white"><?php echo $book['title']; ?></h2>
-
-					<div class="flex items-center mt-4 mb-3">
-						<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-						<p class="ml-2 text-sm font-bold text-gray-900 dark:text-white"><?php echo $book['score']; ?></p>
-						<span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-						<a href="#reviews" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white"><?php echo $book['nb_reviews']; ?> avis</a>
-					</div>
 				<div class="flex items-center mt-4 mb-3">
 					<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
 					<p class="ml-2 text-sm font-bold text-gray-900 dark:text-white"><?php echo $book['score'] === 0 ? 'Aucune note' : $book['score']; ?></p>
@@ -208,22 +205,6 @@ $book = Database::get_single_book( $book_id );
 			</div>
 		  </section>
 
-
-				// 		foreach ($star_inputs as $input) {
-				// 			$score += 1;
-				// 			if ($input == "on") {
-				// 				break;
-				// 			}
-				// 		}
-
-				// 		try {
-				// 			Database::add_review($comment, $book_id, $user_id, $date, $score);
-				// 			echo "<script>alert('Votre commentaire a bien été ajouté !')</script>";
-				// 		} catch (Exception $e) {
-				// 			echo $e->getMessage();
-				// 		}
-				// 	}
-			?>
 
 
 
