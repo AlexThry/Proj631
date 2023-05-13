875b4c43015e3786966a9114ff17e42c09a76194
Arthur <fantache02@gmail.com>
2023-05-13 16:59:42 +0200

    FEATURE : commenter la base de données

--- Java/ConnectionDatabase.java
+++ Java/ConnectionDatabase.java
@@ -16,15 +16,17 @@ public class ConnectionDatabase {
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/proj631?characterEncoding=utf8", "root", "");
             return conn;
+            //renvoie une erreur en cas d'erreur au niveau du script SQL
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
+            //renvoie une erreur si la librairie jdbc n'est pas installée
         } catch (ClassNotFoundException ex) {
             System.out.println("ClassNotFoundException: " + ex.getMessage());
         }
         return null;
     }
 
-
+    //fonction qui permet d'insérer des lignes dans une base de données SQL
     public void insert(String query, Connection conn){
         try {
             //étape 3: créer l'objet statement
@@ -35,6 +37,7 @@ public class ConnectionDatabase {
             System.out.println(e);
         }
     }
+    //fonction qui permet de renvoyer des données contenues dans une colonne de la base de données
     public ArrayList<String> selectList(String query, Connection conn, String colonne){
         ArrayList<String> tab= new ArrayList<String>();
         // créer l'objet statement
@@ -53,6 +56,7 @@ public class ConnectionDatabase {
         return tab;
 
     }
+    //fonction qui permet de renvoyer des données contenues dans trois colonnes de la base de données
     public ArrayList<String[]> selectList3(String query, Connection conn, String colonne1, String colonne2, String colonne3){
         ArrayList<String[]> tab= new ArrayList<String[]>();
         // créer l'objet statement
@@ -69,8 +73,8 @@ public class ConnectionDatabase {
             System.out.println(e);
         }
         return tab;
-
     }
+    //fonction qui  permet de supprimer une ligne d'une base de données
     public void delete(String query, Connection conn){
         try{
         Statement stmt = conn.createStatement();
@@ -80,6 +84,7 @@ public class ConnectionDatabase {
             System.out.println(e);
         }
     }
+    //fonction qui permet de se déconnecter de la base de données
     public void closeConnect(Connection conn){
         try{
             //étape 5: fermez l'objet de connexion
