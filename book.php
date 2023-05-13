c268220ccfb7c9de9de37502525e5796197ce8e7
Alexis Thierry <alexist103@gmail.com>
2023-05-13 13:48:31 +0200

    FEATURE : ajout de filtre de caracteres pour les commentaires

--- book.php
+++ book.php
@@ -26,7 +26,7 @@ $book = Database::get_single_book( $book_id );
 					<h2 class="mb-2 text-5xl font-bold leading-none text-gray-900 md:text-5xl dark:text-white"><?php echo $book['title']; ?></h2>
 				<div class="flex items-center mt-4 mb-3">
 					<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-					<p class="ml-2 text-sm font-bold text-gray-900 dark:text-white"><?php echo $book['score'] === 0 ? 'Aucune note' : $book['score']; ?></p>
+					<p class="ml-2 text-sm font-bold text-gray-900 dark:text-white"><?php echo $book['score'] === 0 ? 'Aucune note' : round($book['score'], 1); ?></p>
 					<span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
 					<a href="#reviews" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white"><?php echo $book['nb_reviews']; ?> avis</a>
 				</div>
@@ -221,12 +221,13 @@ $book = Database::get_single_book( $book_id );
 					&& isset($_POST['star-input3']) && !empty($_POST['star-input3']) && key_exists('star-input3', $_POST)
 					&& isset($_POST['star-input4']) && !empty($_POST['star-input4']) && key_exists('star-input4', $_POST)
 					&& isset($_POST['star-input5']) && !empty($_POST['star-input5']) && key_exists('star-input5', $_POST)) {
-						$comment = $_POST['comment'];
+						$unflitered_comment = $_POST['comment'];
 						$user_id = get_user()['id'];
 						$date = date('Y-m-d', strtotime('today'));
 						$score = 0;
 						$star_inputs = array($_POST['star-input1'],$_POST['star-input2'],$_POST['star-input3'],$_POST['star-input4'],$_POST['star-input5']);
-
+						$patern = '/[<>\'\"%&()=+]/';
+						$comment = preg_replace($patern, "", $unflitered_comment);
 
 						foreach ($star_inputs as $input) {
 							$score += 1;
