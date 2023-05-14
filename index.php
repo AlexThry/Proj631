3ec5a1b1e20a82d2f6a1f15f129905e37e42c8d1
Alexis Thierry <alexist103@gmail.com>
2023-05-14 18:55:38 +0200

    Clean : Suppression des copiés collés

--- index.php
+++ index.php
@@ -60,18 +60,6 @@
 
 	<?php require_once 'includes/search-results.php'; ?>
 
-	<div class="bg-gray-100 dark:bg-gray-900 py-12 sm:py-16 mb-16">
-		<div class="mx-auto max-w-7xl px-6 lg:px-8">
-			<h2 class="text-center text-lg font-semibold leading-8 text-gray-900 dark:text-white">Les équipes les plus innovantes du monde lui font confiance</h2>
-			<div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-				<img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:bg-white dark:rounded" src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg" alt="Transistor" width="158" height="48">
-				<img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:bg-white dark:rounded" src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg" alt="Reform" width="158" height="48">
-				<img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:bg-white dark:rounded" src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg" alt="Tuple" width="158" height="48">
-				<img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 dark:bg-white dark:rounded lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg" alt="SavvyCal" width="158" height="48">
-				<img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1 dark:bg-white dark:rounded" src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg" alt="Statamic" width="158" height="48">
-			</div>
-		</div>
-	</div>
 
 </div>
 
